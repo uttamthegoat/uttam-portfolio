440cc024c3c21ab97e8cb59334d332b169b74ccc
uttamthegoat <19516uttam@gmail.com>
2024-02-21 22:56:04 +0530

    completed navbar animation

--- src/assets/constants.jsx
+++ src/assets/constants.jsx
@@ -1,8 +1,8 @@
 export const navbarItems = [
-  { id: 1, navItem: "Home", to: "/" },
-  { id: 2, navItem: "About", to: "/about" },
-  { id: 3, navItem: "Contact", to: "/contact" },
-  { id: 4, navItem: "Education", to: "/education" },
-  { id: 5, navItem: "Projects", to: "/projects" },
-  { id: 6, navItem: "Blog", to: "/blog" },
+  { id: 1, navItem: "Home", to: "/", stagger: 0 },
+  { id: 2, navItem: "About", to: "/about", stagger: 0.2 },
+  { id: 3, navItem: "Contact", to: "/contact", stagger: 0.4 },
+  { id: 4, navItem: "Education", to: "/education", stagger: 0.6 },
+  { id: 5, navItem: "Projects", to: "/projects", stagger: 0.8 },
+  { id: 6, navItem: "Blog", to: "/blog", stagger: 1 },
 ];
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,28 +3,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { navbarItems } from "../../assets/constants";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className="navbar fixed top-0 left-0 w-[100vw] md:w-[336px] h-[100vh] flex flex-col md:rounded-e-3xl hidden">
+    <div className="navbar fixed top-0 left-0 w-[100vw] md:w-[336px] h-[100vh] flex flex-col md:rounded-e-3xl">
       <div className="close-navbar py-10 flex justify-end px-6">
-        <FontAwesomeIcon
-          icon={faXmark}
-          className="text-white bg-green-200 text-3xl px-3 py-2 cursor-pointer rounded-md"
-        />
+        <motion.div
+          initial={{ x: -30, opacity: 0.1 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+        >
+          <FontAwesomeIcon
+            icon={faXmark}
+            className="text-white bg-green-200 text-3xl px-3 py-2 cursor-pointer rounded-md"
+          />
+        </motion.div>
       </div>
-      <div className="nav-Items flex flex-col items-center space-y-9">
+      <div className="nav-Items flex flex-col items-center">
         {navbarItems.map((link) => (
-          <Link
+          <motion.div
             key={link.id}
-            to={link.to}
-            className="rounded-lg px-3.5 py-2 m-1 overflow-hidden relative group hover:cursor-pointer border-2 font-medium border-green-200 text-white w-7/12"
+            initial={{ x: -200, opacity: 0.1 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 0.5,
+              delay: link.id * 0.17,
+              ease: "easeInOut", // Adjust easing as needed
+            }}
+            className="w-7/12 block mb-10 rounded-lg border-2 border-green-200 py-3 px-4 text-center cursor-pointer hover:bg-green-200 text-green-200 hover:text-black font-bold"
           >
-            <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-green-200 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-            <span className="relative text-green-200 transition duration-300 group-hover:text-black ease text-center block cursor-pointer">
+            <Link key={link.id} to={link.to} className="">
               {link.navItem}
-            </span>
-          </Link>
+            </Link>
+          </motion.div>
         ))}
       </div>
     </div>
